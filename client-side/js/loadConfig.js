84cdf1071b92e0f68379e16e2e45883b7c97ebd9
jjc914 <jjc914@student.cis.edu.hk>
2022-02-22 17:06:45 +0800

    updated request.js

--- client-side/js/loadConfig.js
+++ client-side/js/loadConfig.js
@@ -12,11 +12,10 @@ if (req.status == 200) {
 function load(text) {
   lines = text.split('\n');
   for (let i = 0; i < lines.length; i++) {
-    let line = lines[i].replace(/^\s+|\s+$/g, '')
+    let line = lines[i].replace(/^\s+|\s+$/g, '');
     if (line == '') continue;
     if (line.charAt(0) == '#') continue;
     let data = line.split('=');
-    config[data[0].replace(/^\s+|\s+$/g, '')] = data[1].replace(/^\s+|\s+$/g, '')
+    config[data[0].replace(/^\s+|\s+$/g, '')] = data[1].replace(/^\s+|\s+$/g, '');
   }
-  console.log(config)
 }
